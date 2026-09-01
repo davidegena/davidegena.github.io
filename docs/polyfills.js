907b52fef0e6a41900b36ca701e4fe62fb2aaa3d
Davide Gena <davide.gena@gmail.com>
2026-09-01 18:18:15 +0200

    bug fix

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\david\Desktop\GitHub\davidg33k.github.io\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! c:\Users\david\Desktop\GitHub\davidg33k.github.io\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
